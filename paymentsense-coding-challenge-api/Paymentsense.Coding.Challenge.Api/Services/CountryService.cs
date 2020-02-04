d706c05b0cae8a975fa944b8efea34fe083b3195
Kelvin Penus <kelvinpenus@gmail.com>
2020-02-05 01:10:42 +1100

    Create services to get list of countries and country info

--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Services/CountryService.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Services/CountryService.cs
@@ -1,10 +1,47 @@
-﻿using System;
+﻿using Paymentsense.Coding.Challenge.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
 namespace Paymentsense.Coding.Challenge.Api.Services
 {
-	public class CountryService
-	{
-		public CountryService()
-		{
-		}
-	}
+    public class CountryService
+    {
+        public HttpClient Client { get; }
+
+        public CountryService(HttpClient client)
+        {
+            client.BaseAddress = new Uri("https://restcountries.eu/rest/v2");
+            client.DefaultRequestHeaders.Add("Accept",
+                "application/json");
+            Client = client;
+        }
+
+        public async Task<IEnumerable<Country>> GetCountries()
+        {
+            var response = await Client.GetAsync(
+                "/all?fields=alpha3code;name;flag");
+
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync
+                <IEnumerable<Country>>(responseStream);
+        }
+
+        public async Task<CountryInfo> GetCountryInfo(string alpha3Code)
+        {
+            var response = await Client.GetAsync(
+                string.Format("https://restcountries.eu/rest/v2/alpha/{0}", alpha3Code));
+
+            response.EnsureSuccessStatusCode();
+
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            return await JsonSerializer.DeserializeAsync
+                <CountryInfo>(responseStream);
+        }
+    }
 }
--- paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Startup.cs
+++ paymentsense-coding-challenge-api/Paymentsense.Coding.Challenge.Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Paymentsense.Coding.Challenge.Api.Services;
 
 namespace Paymentsense.Coding.Challenge.Api
 {
@@ -19,6 +20,7 @@ namespace Paymentsense.Coding.Challenge.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHttpClient<CountryService>();
             services.AddHealthChecks();
             services.AddCors(options =>
             {
